65b53cfa237c7579a5aa1aeced5e56c52fff9815
nellie szeplaki <nellieszeplaki@gmail.com>
2026-03-17 13:38:42 +0000

    Switched to UbiqScene and added networking for tools

--- Assets/ScaleAndResetOnGrab.cs
+++ Assets/ScaleAndResetOnGrab.cs
@@ -1,5 +1,7 @@
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
+using Ubiq.Messaging;
+using Ubiq.Geometry;
 
 [RequireComponent(typeof(UnityEngine.XR.Interaction.Toolkit.Interactables.XRGrabInteractable))]
 public class ScaleAndResetOnGrab : MonoBehaviour
@@ -8,17 +10,89 @@ public class ScaleAndResetOnGrab : MonoBehaviour
     private Vector3 originalScale;
     private Vector3 originalPosition;
     private Quaternion originalRotation;
-
     [Range(0.01f, 1f)]
-    public float grabScaleMultiplier = 0.1f; // 25% of original size
+    public float grabScaleMultiplier = 0.1f; 
+
+    private UnityEngine.XR.Interaction.Toolkit.Interactables.XRGrabInteractable grab;
+    private Rigidbody body;
+
+    // Networking
+    private NetworkContext context;
+    private Pose lastPose;
+    private Vector3 lastScale;
+    private bool forceSend;
+
+    private struct Message
+    {
+        public Pose pose;
+        public Vector3 scale;
+    }
+
+    public void ProcessMessage(ReferenceCountedSceneGraphMessage message)
+    {
+        var m = message.FromJson<Message>();
+        var pose = Transforms.ToWorld(m.pose, context.Scene.transform);
+
+        if (body)
+        {
+            body.position = pose.position;
+            body.rotation = pose.rotation;
+        }
+        else
+        {
+            transform.SetPositionAndRotation(pose.position, pose.rotation);
+        }
+
+        transform.localScale = m.scale;
+
+        // Prevent echoing received updates.
+        lastPose = m.pose;
+        lastScale = m.scale;
+    }
+
+    private void SendNetworkUpdates()
+    {
+        if (!grab)
+        {
+            return;
+        }
+
+        // Only the peer currently grabbing this object should publish motion.
+        // Also allow one send right after release/scale changes via forceSend.
+        if (!grab.isSelected && !forceSend)
+        {
+            return;
+        }
+
+        var pose = Transforms.ToLocal(transform, context.Scene.transform);
+        var scale = transform.localScale;
+
+        if (!forceSend && pose.Equals(lastPose) && scale == lastScale)
+        {
+            return;
+        }
+
+        forceSend = false;
+        lastPose = pose;
+        lastScale = scale;
+
+        context.SendJson(new Message
+        {
+            pose = pose,
+            scale = scale
+        });
+    }
 
     void Start()
     {
+        context = NetworkScene.Register(this);
+        
         originalScale = transform.localScale;
         originalPosition = transform.position;
         originalRotation = transform.rotation;
 
-        var grab = GetComponent<UnityEngine.XR.Interaction.Toolkit.Interactables.XRGrabInteractable>();
+        body = GetComponent<Rigidbody>();
+        grab = GetComponent<UnityEngine.XR.Interaction.Toolkit.Interactables.XRGrabInteractable>();
         grab.activated.AddListener(OnGrab);
         grab.deactivated.AddListener(OnRelease);
 
@@ -27,6 +101,11 @@ public class ScaleAndResetOnGrab : MonoBehaviour
 
         Debug.Log("shovel positions saved");
     }   
+
+    private void LateUpdate()
+    {
+        SendNetworkUpdates();
+    }
 
     void OnGrab(ActivateEventArgs eventArgs)
     {
@@ -41,6 +120,7 @@ public class ScaleAndResetOnGrab : MonoBehaviour
     private void XRGrabInteractable_SelectEntered(SelectEnterEventArgs eventArgs)
     {
         transform.localScale = originalScale * grabScaleMultiplier;
+        forceSend = true; // replicate scale change immediately
         Debug.Log("shovel grabbed");
     }
 
@@ -49,6 +129,7 @@ public class ScaleAndResetOnGrab : MonoBehaviour
         transform.localScale = originalScale;
         transform.position = originalPosition;
         transform.rotation = originalRotation;
+        forceSend = true; // replicate reset after release
         Debug.Log("shovel released");
     }
 }
